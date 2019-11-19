8ebb5b470a2169b1e7c0f6e7f5fa8039fe444028
marco.mirandola@coolorange.com <marco.mirandola@coolorange.com>
2019-11-19 06:01:09 -0800

    added further code samples

---
+++ CodeSamples/ListAllDrawings/Vault.Custom/addinVault/Default.ps1
@@ -0,0 +1,24 @@
+﻿
+function OnTabContextChanged
+{
+	$xamlFile = [System.IO.Path]::GetFileName($VaultContext.UserControl.XamlFile)
+
+	ListAllDrawings -xamlFile $xamlFile #call the function for collecting all drawings
+
+	if ($VaultContext.SelectedObject.TypeId.SelectionContext -eq "FileMaster" -and $xamlFile -eq "CAD BOM.xaml")
+	{
+		$fileMasterId = $vaultContext.SelectedObject.Id
+		$file = $vault.DocumentService.GetLatestFileByMasterId($fileMasterId)
+		$bom = @(GetFileBOM($file.id))
+		$dsWindow.FindName("bomList").ItemsSource = $bom
+	}
+	if ($VaultContext.SelectedObject.TypeId.SelectionContext -eq "ItemMaster" -and $xamlFile -eq "Associated Files.xaml")
+	{
+		$items = $vault.ItemService.GetItemsByIds(@($vaultContext.SelectedObject.Id))
+		$item = $items[0]
+		$itemids = @($item.Id)
+		$assocFiles = @(GetAssociatedFiles $itemids $([System.IO.Path]::GetDirectoryName($VaultContext.UserControl.XamlFile)))
+		$dsWindow.FindName("AssoicatedFiles").ItemsSource = $assocFiles
+	}
+}
+
---
+++ CodeSamples/ListAllDrawings/Vault.Custom/addinVault/ListAllDrawings.ps1
@@ -0,0 +1,18 @@
+﻿
+function ListAllDrawings($xamlFile){
+	if ($VaultContext.SelectedObject.TypeId.SelectionContext -eq "FileMaster" -and $xamlFile -eq "Drawings.xaml"){
+		$selectedFileId = $vaultContext.SelectedObject.Id
+		#$file = $vault.DocumentService.GetLatestFileByMasterId($selectedFileId)
+		$fileAssocs = $vault.DocumentService.GetLatestFileAssociationsByMasterIds(@($selectedFileId),"None",$false,"Dependency",$true,$true,$false,$false)
+		$drawings = @()
+		if($null -ne $fileAssocs[0].FileAssocs){
+			foreach($assoc in $fileAssocs[0].FileAssocs){
+				if($assoc.ParFile.Name.EndsWith(".idw")){
+					$drawings += $assoc.ParFile
+				}
+			}
+		}
+		$dsWindow.FindName("ListOfDrawings").ItemsSource = $drawings
+	}
+}
+
